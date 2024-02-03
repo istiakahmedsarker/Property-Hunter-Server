d72da2c56fdf7e8eb089ab443ef3b707a12585f1
raselsharif <raselsharif9@gmail.com>
2024-02-03 13:00:52 +0600

    create api for buyer inquiry post and get all

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const propertyRouter = require("./routes/propertyRoutes");
 const commentRouter = require("./routes/commentRoutes");
 const apartmentRouter = require("./routes/apartmentRoutes");
 const paymentRouter = require("./routes/paymentRoutes");
+const buyerInquiry = require("./routes/buyerInqueryRouters");
 
 // initial server start
 app.get("/", (req, res) => {
@@ -24,6 +25,7 @@ app.use("/api/v1/properties", propertyRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/apartments", apartmentRouter);
 app.use("/api/v1/payments", paymentRouter);
+app.use("/api/v1/buyer-inquiries", buyerInquiry);
 
 // handle error for unknown routes
 app.all("*", (req, res, next) => {
