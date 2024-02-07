d16d02124be2346b03e9ed790088f67f76092f38
IsaT <rakiib010@gmail.com>
2024-02-07 17:26:03 +0600

    [fixSchema](propertySchema):change property schema

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require('express');
 const {
   getAllUser,
   createUser,
   deleteUser,
   getSingleUser,
   makeAdmin,
-} = require("../controller/userController");
+  getSingleUserWithEmail,
+} = require('../controller/userController');
 const router = express.Router();
 
-router.route("/").get(getAllUser).post(createUser);
-router.route("/:id").get(getSingleUser).delete(deleteUser);
-router.route("/make-admin/:id").put(makeAdmin);
+router.route('/').get(getAllUser).post(createUser);
+router.route('/:id').get(getSingleUser).delete(deleteUser);
+router.route('/make-admin/:id').put(makeAdmin);
+router.route('/email/:email').get(getSingleUserWithEmail);
 
 module.exports = router;
--- src/schema/propertyModel.js
+++ src/schema/propertyModel.js
@@ -1,40 +1,118 @@
 const mongoose = require('mongoose');
 
 const propertySchema = new mongoose.Schema({
-  // apartmentImage: {
-  //   type: [String],
-  //   required: [true, 'A property must have some images'],
-  // },
-  // propertyType: {
-  //   type: String,
-  //   required: [true, 'A property must have type'],
-  // },
-  // price: {
-  //   type: Number,
-  //   required: [true, 'A property must have a price'],
-  // },
-  // owner: {
-  //   type: String,
-  //   required: [true, 'A property must have owner'],
-  // },
-  // apartmentNumber: {
-  //   type: String,
-  //   required: [true, 'A apartment must have apartment number'],
-  // },
-  // blockName: {
-  //   type: String,
-  //   required: [true, 'A apartment must have block'],
-  // },
-  // room: {
-  //   type: Number,
-  //   required: [true, 'A apartment must have room'],
-  // },
-  // floorNumber: {
-  //   type: Number,
-  //   required: [true, 'A apartment must have floor number'],
-  // },
-  // latitude: Number,
-  // longtitude: Number,
+  propertyTitle: {
+    type: String,
+    required: [true, 'Property title is required'],
+  },
+  propertyType: {
+    type: String,
+    required: [true, 'Property type is required'],
+  },
+  location: {
+    address: {
+      type: String,
+      required: [true, 'Location address is required'],
+    },
+    city: {
+      type: String,
+      required: [true, 'City is required'],
+    },
+    state: {
+      type: String,
+      required: [true, 'State is required'],
+    },
+    zipCode: {
+      type: String,
+      required: [true, 'Zip code is required'],
+    },
+    latitude: {
+      type: Number,
+      required: [true, 'Latitude is required'],
+    },
+    longitude: {
+      type: Number,
+      required: [true, 'Longitude is required'],
+    },
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  squareFootage: {
+    type: Number,
+    required: [true, 'Square footage is required'],
+  },
+  easement: {
+    type: [String],
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required'],
+  },
+  propertyImages: {
+    type: [String],
+    required: [true, 'Property images are required'],
+  },
+  utilities: {
+    type: [String],
+  },
+  parking: {
+    included: {
+      type: Boolean,
+    },
+    spaces: {
+      type: Number,
+    },
+  },
+  rooms: {
+    bedRooms: {
+      type: Number,
+    },
+    bathRooms: {
+      type: Number,
+    },
+    officeRooms: {
+      type: Number,
+    },
+    conferenceRooms: {
+      type: Number,
+    },
+  },
+  floorNumber: {
+    type: Number,
+  },
+  blockName: {
+    type: String,
+  },
+  apartmentNumber: {
+    type: String,
+  },
+  yearBuilt: {
+    type: Number,
+    required: [true, 'Year built is required'],
+  },
+  propertyStatus: {
+    type: String,
+    required: [true, 'Property status is required'],
+  },
+  ownerInformation: {
+    name: {
+      type: String,
+      required: [true, 'Owner name is required'],
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email),
+        message: 'Invalid email format',
+      },
+    },
+    phone: {
+      type: String,
+    },
+  },
 });
 
 const Property = mongoose.model('Property', propertySchema);
