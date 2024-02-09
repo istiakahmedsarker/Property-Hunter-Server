1324be07bab9e905b6f012f8870882d888fd40f5
raselsharif <raselsharif9@gmail.com>
2024-02-10 01:42:29 +0600

    fixed like dislike route

--- src/routes/likeDislikeCountsRoutes.js
+++ src/routes/likeDislikeCountsRoutes.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
-    increaseLike,
-    increaseDislike,
-    decreaseLike,
-    decreaseDislike,
-} = require('./path-to-your-like-dislike-module'); // Update the path accordingly
+  increaseLike,
+  increaseDislike,
+  decreaseLike,
+  decreaseDislike,
+} = require("../controller/likeDislikeCountsController"); // Update the path accordingly
 
 // Increase Like
-router.patch('/increase-like/:id', increaseLike);
+router.patch("/increase-like/:id", increaseLike);
 
 // Increase Dislike
-router.patch('/increase-dislike/:id', increaseDislike);
+router.patch("/increase-dislike/:id", increaseDislike);
 
 // Decrease Like
-router.patch('/decrease-like/:id', decreaseLike);
+router.patch("/decrease-like/:id", decreaseLike);
 
 // Decrease Dislike
-router.patch('/decrease-dislike/:id', decreaseDislike);
+router.patch("/decrease-dislike/:id", decreaseDislike);
 
 module.exports = router;
