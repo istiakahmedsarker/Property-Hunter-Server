d6407a25e0a50b945d7fb1b5d44203d13f763a11
raselsharif <raselsharif9@gmail.com>
2024-02-10 00:00:21 +0600

    update favorite schema

--- src/schema/propertyFavoriteModel.js
+++ src/schema/propertyFavoriteModel.js
@@ -1,13 +1,29 @@
 const mongoose = require("mongoose");
 
 const propertyFavoriteSchema = new mongoose.Schema({
+  property_id: {
+    type: String,
+    required: [true, "Property ID is required"],
+  },
+  property_title: {
+    type: String,
+    required: [true, "Property title is required"],
+  },
   user_email: {
     type: String,
     required: [true, "User email is required"],
   },
-  property_id: {
+  property_images: {
     type: String,
-    required: [true, "Property ID is required"],
+    required: [true, "Property image is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Property price is required"],
+  },
+  property_location: {
+    type: String,
+    required: [true, "Property location is required"],
   },
 });
 
