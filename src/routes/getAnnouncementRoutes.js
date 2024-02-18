ae74f9093c11d5aa0d02b184cf767915db7feaa4
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-18 12:43:21 +0600

    [feat](changePropertyStatus) created api route to change property status

--- src/routes/getAnnouncementRoutes.js
+++ src/routes/getAnnouncementRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router()
 const getAnnouncementController = require('../controller/getAnnouncementController')
 
-router.get('/getAnnouncement',getAnnouncementController)
+router.get('/getAnnouncement',getAnnouncementController.getAnnouncements)
 
 module.exports = router;
--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -3,10 +3,12 @@ const {
   getAllProperty,
   createProperty,
   getSingleProperty,
+  changePropertyStatus
 } = require('../controller/propertyController');
 const router = express.Router();
 
 router.route('/').get(getAllProperty).post(createProperty);
 router.route('/:id').get(getSingleProperty);
+router.route('/changePropertyStatus/:id').patch(changePropertyStatus);
 
 module.exports = router;
