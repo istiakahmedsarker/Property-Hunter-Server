3be80d21358b6517f6c26965961718d29fc32ef8
raselsharif <raselsharif9@gmail.com>
2024-02-24 20:33:42 +0600

    fix duplicate variable in app.js file and install jsonwebtoken

--- src/app.js
+++ src/app.js
@@ -1,36 +1,21 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const stripe = require('stripe')(process.env.STRIPE_KEY);
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(express.json());
 
-const blogRouter = require('./routes/blogRoutes');
-const propertyRouter = require('./routes/propertyRoutes');
-const commentRouter = require('./routes/commentRoutes');
-const userRouter = require('./routes/userRoutes');
-const paymentRouter = require('./routes/paymentRoutes');
-const buyerInquiry = require('./routes/buyerInqueryRouters');
-const propertyFavorite = require('./routes/propertyFavoriteRouters');
-const likeDislikeRoutes = require('./routes/likeDislikeCountsRoutes');
-const announcementRouters = require('./routes/annoucementRouters');
-const getFeaturedPropertiesRoutes = require('./routes/getFeaturedPropertiesRoutes');
-const getAnnouncementRoutes = require('./routes/getAnnouncementRoutes');
-const subscriberRouters = require('./routes/subscribersRoutes');
-const sendEmailRouter = require('./routes/sendEmailRouters');
-const authRouter = require('./routes/authRouter');
 const blogRouter = require("./routes/blogRoutes");
 const propertyRouter = require("./routes/propertyRoutes");
 const commentRouter = require("./routes/commentRoutes");
-// const apartmentRouter = require("./routes/apartmentRoutes");
 const userRouter = require("./routes/userRoutes");
 const paymentRouter = require("./routes/paymentRoutes");
 const buyerInquiry = require("./routes/buyerInqueryRouters");
@@ -41,39 +26,42 @@ const getFeaturedPropertiesRoutes = require("./routes/getFeaturedPropertiesRoute
 const getAnnouncementRoutes = require("./routes/getAnnouncementRoutes");
 const subscriberRouters = require("./routes/subscribersRoutes");
 const sendEmailRouter = require("./routes/sendEmailRouters");
+const authRouter = require("./routes/authRouter");
+// const apartmentRouter = require("./routes/apartmentRoutes");
+
 // initial server start
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res
     .status(200)
-    .json({ message: 'Server is running', app: 'Property Hunter' });
+    .json({ message: "Server is running", app: "Property Hunter" });
 });
 
 // routes mounting
-app.use('/api/v1/blogs', blogRouter);
-app.use('/api/v1/properties', propertyRouter);
-app.use('/api/v1/comments', commentRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/payments', paymentRouter);
-app.use('/api/v1/buyer-inquiries', buyerInquiry);
-app.use('/api/v1/property-favorite', propertyFavorite);
-app.use('/api/v1/like-dislike', likeDislikeRoutes);
-app.use('/api/v1/announcement', announcementRouters);
-app.use('/api/v1/getFeaturedProperties', getFeaturedPropertiesRoutes);
-app.use('/api/v1/getAnnouncement', getAnnouncementRoutes);
-app.use('/api/v1/subscriber', subscriberRouters);
-app.use('/api/v1/send-emails', sendEmailRouter);
-app.use('/api/v1/jwt', authRouter);
+app.use("/api/v1/blogs", blogRouter);
+app.use("/api/v1/properties", propertyRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/payments", paymentRouter);
+app.use("/api/v1/buyer-inquiries", buyerInquiry);
+app.use("/api/v1/property-favorite", propertyFavorite);
+app.use("/api/v1/like-dislike", likeDislikeRoutes);
+app.use("/api/v1/announcement", announcementRouters);
+app.use("/api/v1/getFeaturedProperties", getFeaturedPropertiesRoutes);
+app.use("/api/v1/getAnnouncement", getAnnouncementRoutes);
+app.use("/api/v1/subscriber", subscriberRouters);
+app.use("/api/v1/send-emails", sendEmailRouter);
+app.use("/api/v1/jwt", authRouter);
 
 // payment stripe
 
-app.post('/api/v1/create-payment-intent', async (req, res) => {
+app.post("/api/v1/create-payment-intent", async (req, res) => {
   const { price } = req.body;
   const amount = parseInt(price * 100);
-  console.log('amount', amount);
+  console.log("amount", amount);
   const paymentIntent = await stripe.paymentIntents.create({
     amount: amount,
-    currency: 'usd',
-    payment_method_types: ['card'],
+    currency: "usd",
+    payment_method_types: ["card"],
   });
   res.send({
     clientSecret: paymentIntent.client_secret,
@@ -81,9 +69,9 @@ app.post('/api/v1/create-payment-intent', async (req, res) => {
 });
 
 // handle error for unknown routes
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   res.status(404).json({
-    status: 'fail',
+    status: "fail",
     message: `Can't find ${req.originalUrl} on this server`,
   });
 });
